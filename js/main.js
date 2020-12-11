d6d047222fe98640d110f8c1f8d3c4e99a063eed
pmacovei75 <75494510+pmacovei75@users.noreply.github.com>
2020-12-11 18:54:24 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var data = new Date ();
+document.getElementById('an').innerHTML = data.getFullYear();
+
+
+
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className = "nav mobile";
+    } else {
+        x.className = "nav";
+    }
+}
